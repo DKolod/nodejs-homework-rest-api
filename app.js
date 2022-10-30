87022444808826f2a4be623a278921083d611711
Dmitry Kolodchenko <92672116+DKolod@users.noreply.github.com>
2022-10-30 21:57:28 +0200

    update index

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const { unknownRouteHandler, errorHandler } = require("./helpers");
 
 const { contactsRouter, authRouter } = require("./routes");
 
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+const { unknownRouteHandler, errorHandler } = require("./helpers");
+
+const { contactsRouter, authRouter, usersRouter } = require("./routes");
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
 
 app.use(logger(formatsLogger));
 app.use(cors());
@@ -17,7 +24,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/api/contacts", contactsRouter);
-app.use("/api/users", authRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
+
 app.use(unknownRouteHandler);
 app.use(errorHandler);
 
